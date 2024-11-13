c629cd9988408554543b6a1ec8983b82a72f5e8f
henryrong <henryrong@holtek.com.tw>
2024-11-13 10:03:33 +0800

    update

--- src/main/java/com/test/RadioTestVM.java
+++ src/main/java/com/test/RadioTestVM.java
@@ -1,16 +1,9 @@
 package com.test;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
-import org.zkoss.zk.ui.Executions;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 public class RadioTestVM {
 	private String workOnType;
@@ -24,47 +17,6 @@ public class RadioTestVM {
 		this.setWorkOnType("2");
 		
 		this.setWorkOffType("1");
-		
-		HttpServletRequest request = (HttpServletRequest) Executions.getCurrent().getNativeRequest();
-		source = request.getParameter("source");
-		queryType = request.getParameter("queryType");
-		System.out.println(request.getRequestURI());
-		System.out.println(request.getQueryString());
-		
-		Map<String, String> paramMap = genParameterMapByQueryString(request.getQueryString());
-		if(paramMap != null) {
-			for (Map.Entry<String, String> entry : paramMap.entrySet()) {
-				System.out.println("key:"+entry.getKey()+", value:"+entry.getValue());
-			}
-		}
-		
-		source = paramMap.get("source");
-		queryType = paramMap.get("queryType");
-	}
-	
-	private Map<String, String> genParameterMapByQueryString(String queryString) {
-		Map<String, String> result = new HashMap<String, String>();
-		if(StringUtils.isNotEmpty(queryString)) {
-			queryString = StringUtils.replace(queryString, "&amp;", "&");
-			String[] tokens = queryString.split("&");
-			if(tokens != null && tokens.length > 0) {
-				String[] tempKeyValueTokens;
-				for(String token : tokens) {
-					tempKeyValueTokens = token.split("=");
-					if(tempKeyValueTokens != null && tempKeyValueTokens.length > 1 && result.get(tempKeyValueTokens[0]) == null) {
-						result.put(tempKeyValueTokens[0], tempKeyValueTokens[1]);
-					}
-				}
-			}
-		}
-		return result;
-	}
-	
-	@Command
-	public void onClickBtn() {
-		String url = "index.zul?queryType=ACTIVE&source=ZK";
-		
-		Executions.getCurrent().sendRedirect(url);
 	}
 	
 	@Command
